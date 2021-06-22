6f28ed3b12166ccf123440a0d243d3324ca2498e
MaksymRudnytskiy <therudmax@gmail.com>
2021-06-22 20:26:48 +0300

    post 3

---
+++ src/components/post/AppPost.js
@@ -0,0 +1,17 @@
+//
+// import './App.css';
+// import Post from "./components/post/Post";
+//
+// function App() {
+//
+//
+//     return (
+//         <div>
+//             {
+//                 <Post/>
+//             }
+//         </div>
+//     );
+// }
+//
+// export default App;
