6a19648fa9944e10b9585ea931ba6958a148a5ae
MaksymRudnytskiy <therudmax@gmail.com>
2021-06-29 16:44:50 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 
-import './App.css';
-import Post from "./components/post/Post";
-import Users from "./components/users/Users";
 
 function App() {
 
 
   return (
     <div>
-      {
-        <Users/>
-      }
+
     </div>
   );
 }
